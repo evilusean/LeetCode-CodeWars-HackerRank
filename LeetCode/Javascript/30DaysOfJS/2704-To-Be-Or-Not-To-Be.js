8fb7694ad6014e9ca63da930d4159fa232f6d3aa
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-23 21:09:25 -0300

    Update 2704-To-Be-Or-Not-To-Be.js

--- LeetCode/Javascript/30DaysOfJS/2704-To-Be-Or-Not-To-Be.js
+++ LeetCode/Javascript/30DaysOfJS/2704-To-Be-Or-Not-To-Be.js
@@ -21,3 +21,13 @@ Input: func = () => expect(5).notToBe(null)
 Output: {"value": true}
 Explanation: 5 !== null so this expression returns true.
 */
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
