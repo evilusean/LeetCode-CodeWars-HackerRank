b6afbcc228f15788ed3184959982d52bec074067
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-28 15:55:14 -0300

    Update 2635-Transform-Element-Array.js

--- LeetCode/Javascript/30DaysOfJS/2635-Transform-Element-Array.js
+++ LeetCode/Javascript/30DaysOfJS/2635-Transform-Element-Array.js
@@ -33,4 +33,10 @@ fn returns a number
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function (arr, fn) {};
+var map = function (arr, fn) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(fn(arr[i], i));
+  }
+  return result;
+};
