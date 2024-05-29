e3569ded3c53e8e814bcc814456f39a5ee55248c
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-29 14:05:05 -0300

    Update 2634-Filter-Element-Array.js

--- LeetCode/Javascript/30DaysOfJS/2634-Filter-Element-Array.js
+++ LeetCode/Javascript/30DaysOfJS/2634-Filter-Element-Array.js
@@ -37,4 +37,12 @@ Constraints:
  * @param {Function} fn
  * @return {number[]}
  */
-var filter = function (arr, fn) {};
+var filter = function (arr, fn) {
+  const filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      filteredArr.push(arr[i]);
+    }
+  }
+  return filteredArr;
+};
