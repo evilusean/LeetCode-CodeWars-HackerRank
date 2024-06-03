75b355696a801acb4b1eb1c8c01cd26107c70113
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-03 05:43:08 -0300

    Update 2703-Return-Length-Arg.js

--- LeetCode/Javascript/30DaysOfJS/2703-Return-Length-Arg.js
+++ LeetCode/Javascript/30DaysOfJS/2703-Return-Length-Arg.js
@@ -29,8 +29,9 @@ args is a valid JSON array
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
-var argumentsLength = function (...args) {};
-
+var argumentsLength = function (...args) {
+  return args.length;
+};
 /**
  * argumentsLength(1, 2, 3); // 3
  */
