825040628b4dc306dc1d408496d3473d52cea749
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-03 15:34:15 -0300

    Update 2666-Allow-One-Function-Call.js

--- LeetCode/Javascript/30DaysOfJS/2666-Allow-One-Function-Call.js
+++ LeetCode/Javascript/30DaysOfJS/2666-Allow-One-Function-Call.js
@@ -3,7 +3,6 @@ Given a function fn, return a new function that is identical to the original fun
 The first time the returned function is called, it should return the same result as fn.
 Every subsequent time it is called, it should return undefined.
  
-
 Example 1:
 Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
 Output: [{"calls":1,"value":6}]
