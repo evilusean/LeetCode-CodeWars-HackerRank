7b9bd3358a393928583c784ce3ec7cf3714dd2d1
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-04 12:14:11 -0300

    Update 2666-Allow-One-Function-Call.js

--- LeetCode/Javascript/30DaysOfJS/2666-Allow-One-Function-Call.js
+++ LeetCode/Javascript/30DaysOfJS/2666-Allow-One-Function-Call.js
@@ -33,9 +33,19 @@ calls is a valid JSON array
  * @return {Function}
  */
 var once = function (fn) {
-  return function (...args) {};
-};
+  let hasBeenCalled = false;
+  let result;
 
+  return function () {
+    if (!hasBeenCalled) {
+      hasBeenCalled = true;
+      result = fn.apply(this, arguments);
+      return result;
+    } else {
+      return undefined;
+    }
+  };
+};
 /**
  * let fn = (a,b,c) => (a + b + c)
  * let onceFn = once(fn)
