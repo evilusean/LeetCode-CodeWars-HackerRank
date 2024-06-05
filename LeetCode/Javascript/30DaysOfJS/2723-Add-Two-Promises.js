c3b8d69a1770fcbe75ab6c48982d9bc19476d289
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-05 05:00:41 -0300

    Update 2723-Add-Two-Promises.js

--- LeetCode/Javascript/30DaysOfJS/2723-Add-Two-Promises.js
+++ LeetCode/Javascript/30DaysOfJS/2723-Add-Two-Promises.js
@@ -22,3 +22,14 @@ Constraints:
 
 promise1 and promise2 are promises that resolve with a number
 */
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function (promise1, promise2) {};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
