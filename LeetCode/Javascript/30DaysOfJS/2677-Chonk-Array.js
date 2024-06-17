f159edbac2f7148c1d3f55580bdc942caf28dd1b
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-17 15:20:36 -0300

    Update 2677-Chonk-Array.js

--- LeetCode/Javascript/30DaysOfJS/2677-Chonk-Array.js
+++ LeetCode/Javascript/30DaysOfJS/2677-Chonk-Array.js
@@ -36,5 +36,9 @@ arr is a valid JSON array
  * @return {Array}
  */
 var chunk = function(arr, size) {
-    
+    let chunked = [];
+    for (let i = 0; i < arr.length; i += size) {
+        chunked.push(arr.slice(i, i + size));
+    }
+    return chunked;
 };
