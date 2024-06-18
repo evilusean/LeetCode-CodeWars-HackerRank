85e17f5ca25705356bd552e7dde84ffb0f5f2f00
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-18 10:36:32 -0300

    Update 2619-Array-ProtoType-Last.js

--- LeetCode/Javascript/30DaysOfJS/2619-Array-ProtoType-Last.js
+++ LeetCode/Javascript/30DaysOfJS/2619-Array-ProtoType-Last.js
@@ -20,15 +20,20 @@ arr is a valid JSON array
 /**
  * @return {null|boolean|number|string|Array|Object}
  */
+// This line defines a new method called 'last' on the 'Array.prototype'. This means that any array in JavaScript will now have access to this 'last' method.
 Array.prototype.last = function() {
+    // This line checks if the array ('this') is empty. 'this.length' represents the number of elements in the array.
     if (this.length === 0) {
+        // If the array is empty ('this.length === 0'), the function returns '-1'.
         return -1;
     } else {
+        // If the array is not empty, this block of code executes.
+        // This line returns the last element of the array.
+        // 'this.length - 1' calculates the index of the last element (remember that array indices start at 0).
+        // 'this[this.length - 1]' accesses the element at that index and returns it.
         return this[this.length - 1];
     }
 };
-
-
 /**
  * const arr = [1, 2, 3];
  * arr.last(); // 3
