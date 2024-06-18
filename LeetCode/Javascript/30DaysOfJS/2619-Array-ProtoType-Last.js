2914da86d4d0d64ebd8790e3c2c16a111f9ad8cd
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-18 10:35:52 -0300

    Update 2619-Array-ProtoType-Last.js

--- LeetCode/Javascript/30DaysOfJS/2619-Array-ProtoType-Last.js
+++ LeetCode/Javascript/30DaysOfJS/2619-Array-ProtoType-Last.js
@@ -21,8 +21,13 @@ arr is a valid JSON array
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function() {
-    
+    if (this.length === 0) {
+        return -1;
+    } else {
+        return this[this.length - 1];
+    }
 };
+
 
 /**
  * const arr = [1, 2, 3];
