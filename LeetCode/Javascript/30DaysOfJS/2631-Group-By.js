203750482a7c63cdbe88044eb4ba3300ce90d5c5
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-19 08:47:46 -0300

    Update 2631-Group-By.js

--- LeetCode/Javascript/30DaysOfJS/2631-Group-By.js
+++ LeetCode/Javascript/30DaysOfJS/2631-Group-By.js
@@ -71,9 +71,16 @@ fn returns a string
  * @return {Object}
  */
 Array.prototype.groupBy = function(fn) {
-    
+    let grouped = {};
+    for (let i = 0; i < this.length; i++) {
+        let key = fn(this[i]);
+        if (grouped[key] === undefined) {
+            grouped[key] = [];
+        }
+        grouped[key].push(this[i]);
+    }
+    return grouped;
 };
-
 /**
  * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
  */
