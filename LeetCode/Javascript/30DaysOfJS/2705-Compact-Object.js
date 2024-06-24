13a146c6341d9f5545aeccdec609ae8dcb817567
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-24 06:40:03 -0300

    Update 2705-Compact-Object.js

--- LeetCode/Javascript/30DaysOfJS/2705-Compact-Object.js
+++ LeetCode/Javascript/30DaysOfJS/2705-Compact-Object.js
@@ -1,6 +1,5 @@
 /*
 Given an object or array obj, return a compact object.
-
 A compact object is the same as the original object, except with keys containing falsy values removed. 
 This operation applies to the object and any nested objects. Arrays are considered objects where the indices are keys. 
 A value is considered falsy when Boolean(value) returns false.
@@ -29,6 +28,21 @@ obj is a valid JSON object
  * @param {Object|Array} obj
  * @return {Object|Array}
  */
-var compactObject = function(obj) {
-    
+var compactObject = function (obj) {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+
+    if (Array.isArray(obj)) {
+        return obj.filter(Boolean).map(compactObject);
+    }
+
+    const result = {};
+    for (const key in obj) {
+        const value = compactObject(obj[key]);
+        if (Boolean(value)) {
+            result[key] = value;
+        }
+    }
+    return result;
 };
