4cd53148f13d087fe6a3fbe3d1ee87be076460c6
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-26 07:52:06 -0300

    Update 2695-Array-Wrapper.js

--- LeetCode/Javascript/30DaysOfJS/2695-Array-Wrapper.js
+++ LeetCode/Javascript/30DaysOfJS/2695-Array-Wrapper.js
@@ -36,22 +36,23 @@ Note: nums is the array passed to the constructor
  * @return {void}
  */
 var ArrayWrapper = function(nums) {
-    
+    this.nums = nums;
 };
 
 /**
  * @return {number}
  */
 ArrayWrapper.prototype.valueOf = function() {
-    
+    return this.nums.reduce((sum, num) => sum + num, 0);
 }
 
 /**
  * @return {string}
  */
 ArrayWrapper.prototype.toString = function() {
-    
+    return `[${this.nums.join(',')}]`;
 }
+
 
 /**
  * const obj1 = new ArrayWrapper([1,2]);
