e9bf6e1abb435ed8d8ea19873021596f4ddf67d2
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-26 06:51:56 -0300

    Update 2695-Array-Wrapper.js

--- LeetCode/Javascript/30DaysOfJS/2695-Array-Wrapper.js
+++ LeetCode/Javascript/30DaysOfJS/2695-Array-Wrapper.js
@@ -31,3 +31,32 @@ Constraints:
 0 <= nums[i] <= 1000
 Note: nums is the array passed to the constructor
 */
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
