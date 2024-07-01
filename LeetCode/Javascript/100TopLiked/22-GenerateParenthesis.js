70a48e17d42dbdfb71ad99e90cddda07c17996e5
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-07-01 11:51:07 -0300

    Update 22-GenerateParenthesis.js

--- LeetCode/Javascript/100TopLiked/22-GenerateParenthesis.js
+++ LeetCode/Javascript/100TopLiked/22-GenerateParenthesis.js
@@ -17,6 +17,25 @@ Constraints:
  * @param {number} n
  * @return {string[]}
  */
-var generateParenthesis = function(n) {
+ar generateParenthesis = function(n) {
+    const result = [];
     
+    const backtrack = (currentString, openCount, closeCount) => {
+        if (currentString.length === 2 * n) {
+            result.push(currentString);
+            return;
+        }
+        
+        if (openCount < n) {
+            backtrack(currentString + '(', openCount + 1, closeCount);
+        }
+        
+        if (closeCount < openCount) {
+            backtrack(currentString + ')', openCount, closeCount + 1);
+        }
+    };
+    
+    backtrack('', 0, 0);
+    
+    return result;
 };
