e1bd5d7709b61fb6b80f0ac111e46722bda76684
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-07-01 02:18:44 -0300

    Update 17-LetterCombosPhoneNumber.js

--- LeetCode/Javascript/100TopLiked/17-LetterCombosPhoneNumber.js
+++ LeetCode/Javascript/100TopLiked/17-LetterCombosPhoneNumber.js
@@ -24,5 +24,36 @@ digits[i] is a digit in the range ['2', '9'].
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
+    const mapping = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    };
     
+    const result = [];
+    
+    if (digits.length === 0) {
+        return result;
+    }
+    
+    const backtrack = (index, currentString) => {
+        if (index === digits.length) {
+            result.push(currentString);
+            return;
+        }
+        
+        const letters = mapping[digits[index]];
+        for (let i = 0; i < letters.length; i++) {
+            backtrack(index + 1, currentString + letters[i]);
+        }
+    };
+    
+    backtrack(0, '');
+    
+    return result;
 };
