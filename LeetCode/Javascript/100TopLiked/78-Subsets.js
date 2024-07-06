3bdedc1dedf7f47c0708f272d2044e6529e86464
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-07-06 08:58:48 -0300

    Update 78-Subsets.js

--- LeetCode/Javascript/100TopLiked/78-Subsets.js
+++ LeetCode/Javascript/100TopLiked/78-Subsets.js
@@ -23,5 +23,12 @@ All the numbers of nums are unique.
  * @return {number[][]}
  */
 var subsets = function(nums) {
-    
+    let result = [[]];
+    for (let i = 0; i < nums.length; i++) {
+        let len = result.length;
+        for (let j = 0; j < len; j++) {
+            result.push([...result[j], nums[i]]);
+        }
+    }
+    return result;
 };
