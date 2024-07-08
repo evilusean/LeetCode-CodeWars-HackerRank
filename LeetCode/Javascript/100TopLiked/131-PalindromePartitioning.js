90bd1e61555d79fa0d34b6f2aac039482b48842d
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-07-08 11:29:11 -0300

    Update 131-PalindromePartitioning.js

--- LeetCode/Javascript/100TopLiked/131-PalindromePartitioning.js
+++ LeetCode/Javascript/100TopLiked/131-PalindromePartitioning.js
@@ -22,5 +22,30 @@ s contains only lowercase English letters.
  * @return {string[][]}
  */
 var partition = function(s) {
-    
+    const result = [];
+    const dfs = (index, currentPartition) => {
+        if (index === s.length) {
+            result.push([...currentPartition]);
+            return;
+        }
+        for (let i = index; i < s.length; i++) {
+            if (isPalindrome(s, index, i)) {
+                currentPartition.push(s.substring(index, i + 1));
+                dfs(i + 1, currentPartition);
+                currentPartition.pop();
+            }
+        }
+    };
+    const isPalindrome = (str, start, end) => {
+        while (start < end) {
+            if (str[start] !== str[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    };
+    dfs(0, []);
+    return result;
 };
