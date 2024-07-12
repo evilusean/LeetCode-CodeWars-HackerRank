67cda6cfebc4dcb02574bab6f6949d3d1cc10034
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-07-12 07:54:56 -0300

    Update 34-FindFirstAndLastPositionElementSortedArray.js

--- LeetCode/Javascript/100TopLiked/34-FindFirstAndLastPositionElementSortedArray.js
+++ LeetCode/Javascript/100TopLiked/34-FindFirstAndLastPositionElementSortedArray.js
@@ -27,5 +27,38 @@ nums is a non-decreasing array.
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
+    let start = -1;
+    let end = -1;
     
+    // Binary search to find the first occurrence of the target
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            start = mid;
+            right = mid - 1; // Search for the first occurrence in the left half
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    // Binary search to find the last occurrence of the target
+    left = 0;
+    right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            end = mid;
+            left = mid + 1; // Search for the last occurrence in the right half
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    return [start, end];
 };
