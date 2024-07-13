785f42a8810b54ada030ceca2f4be723d493ead3
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-07-13 12:58:25 -0300

    Update 35-SearchInsertPosition.js

--- LeetCode/Javascript/100TopLiked/35-SearchInsertPosition.js
+++ LeetCode/Javascript/100TopLiked/35-SearchInsertPosition.js
@@ -27,5 +27,18 @@ nums contains distinct values sorted in ascending order.
  * @return {number}
  */
 var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
     
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
 };
