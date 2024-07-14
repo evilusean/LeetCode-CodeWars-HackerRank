9520d60cf9e514dbeceda181311662fa5b9c563c
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-07-14 07:29:13 -0300

    Update 74-Search2DMatrix.js

--- LeetCode/Javascript/100TopLiked/74-Search2DMatrix.js
+++ LeetCode/Javascript/100TopLiked/74-Search2DMatrix.js
@@ -25,5 +25,24 @@ n == matrix[i].length
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    
+    if (matrix.length === 0 || matrix[0].length === 0) {
+        return false;
+    }
+    let m = matrix.length;
+    let n = matrix[0].length;
+    let left = 0;
+    let right = m * n - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let row = Math.floor(mid / n);
+        let col = mid % n;
+        if (matrix[row][col] === target) {
+            return true;
+        } else if (matrix[row][col] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return false;
 };
