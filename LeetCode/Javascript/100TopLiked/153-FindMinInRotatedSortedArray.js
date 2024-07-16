a98bb1960a253ebb0059dfc743a846f34c12f37d
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-07-16 09:23:34 -0300

    Update 153-FindMinInRotatedSortedArray.js

--- LeetCode/Javascript/100TopLiked/153-FindMinInRotatedSortedArray.js
+++ LeetCode/Javascript/100TopLiked/153-FindMinInRotatedSortedArray.js
@@ -34,5 +34,15 @@ nums is sorted and rotated between 1 and n times.
  * @return {number}
  */
 var findMin = function(nums) {
-    
+    let left = 0;
+    let right = nums.length - 1;
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return nums[left];
 };
