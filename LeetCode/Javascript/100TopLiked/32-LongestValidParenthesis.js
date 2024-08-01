13f3a1b8016c165d8447b26480d95e6a6b8d4adb
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-08-01 12:34:40 -0300

    Update 32-LongestValidParenthesis.js

--- LeetCode/Javascript/100TopLiked/32-LongestValidParenthesis.js
+++ LeetCode/Javascript/100TopLiked/32-LongestValidParenthesis.js
@@ -1,3 +1,26 @@
+/*
+https://leetcode.com/problems/longest-valid-parentheses/description/?envType=study-plan-v2&envId=top-100-liked
+Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses 
+substring.
+
+Example 1:
+Input: s = "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()".
+
+Example 2:
+Input: s = ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()".
+
+Example 3:
+Input: s = ""
+Output: 0
+
+Constraints:
+0 <= s.length <= 3 * 104
+s[i] is '(', or ')'.
+*/
 /**
  * @param {string} s
  * @return {number}
