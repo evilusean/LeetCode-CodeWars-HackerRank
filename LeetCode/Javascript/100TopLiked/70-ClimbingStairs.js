364d6eaf3fa46ffa3b4f78c5e7af3383cdbf20f4
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-08-05 11:53:08 -0300

    Update 70-ClimbingStairs.js

--- LeetCode/Javascript/100TopLiked/70-ClimbingStairs.js
+++ LeetCode/Javascript/100TopLiked/70-ClimbingStairs.js
@@ -26,14 +26,23 @@ Constraints:
  * @return {number}
  */
 var climbStairs = function(n) {
+    // If n is less than or equal to 1, there's only one way to climb (do nothing or take one step).
     if (n <= 1) {
         return 1;
     }
+    // Create a dynamic programming array 'dp' of size n + 1 to store the number of ways to climb to each step.
+    // Each element 'dp[i]' will store the number of ways to climb to the ith step.
     let dp = new Array(n + 1).fill(0);
+    // Initialize dp[1] to 1, as there's only one way to reach step 1 (take one step).
     dp[1] = 1;
+    // Initialize dp[2] to 2, as there are two ways to reach step 2 (take one step twice or take two steps).
     dp[2] = 2;
+    // Iterate through the steps from 3 to n.
     for (let i = 3; i <= n; i++) {
+        // For each step i, the number of ways to climb to it is the sum of the ways to climb to the previous step (i - 1) and the step before that (i - 2).
+        // This is because you can either take one step from i - 1 or two steps from i - 2.
         dp[i] = dp[i - 1] + dp[i - 2];
     }
+    // Return dp[n], which represents the number of ways to climb to the top (nth step).
     return dp[n];
 };
