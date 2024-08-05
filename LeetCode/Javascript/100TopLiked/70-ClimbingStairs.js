51bf5a935b5d5aed0a43ddb69330506b91cb9de4
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-08-05 11:52:34 -0300

    Update 70-ClimbingStairs.js

--- LeetCode/Javascript/100TopLiked/70-ClimbingStairs.js
+++ LeetCode/Javascript/100TopLiked/70-ClimbingStairs.js
@@ -26,5 +26,14 @@ Constraints:
  * @return {number}
  */
 var climbStairs = function(n) {
-    
+    if (n <= 1) {
+        return 1;
+    }
+    let dp = new Array(n + 1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
 };
