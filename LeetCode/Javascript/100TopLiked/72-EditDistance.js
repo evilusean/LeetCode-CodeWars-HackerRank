96fd67017a181caf318b1ebfdcbf0d5a83bb9b02
Sean Teams <shawntemesy@hotmail.com>
2024-08-09 16:43:53 -0300

    Update 72-EditDistance.js

--- LeetCode/Javascript/100TopLiked/72-EditDistance.js
+++ LeetCode/Javascript/100TopLiked/72-EditDistance.js
@@ -1,5 +1,5 @@
 /*
-
+https://leetcode.com/problems/edit-distance/description/?envType=study-plan-v2&envId=top-100-liked
 Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
 You have the following three operations permitted on a word:
 Insert a character
