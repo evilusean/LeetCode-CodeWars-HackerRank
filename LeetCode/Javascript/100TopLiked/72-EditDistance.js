972c21b2f620d8435fae42d00c19859b6cde634c
Sean Teams <shawntemesy@hotmail.com>
2024-08-09 16:53:04 -0300

    Update 72-EditDistance.js

--- LeetCode/Javascript/100TopLiked/72-EditDistance.js
+++ LeetCode/Javascript/100TopLiked/72-EditDistance.js
@@ -34,5 +34,36 @@ word1 and word2 consist of lowercase English letters.
  * @return {number}
  */
 var minDistance = function(word1, word2) {
-    
+    const m = word1.length;
+    const n = word2.length;
+
+    // Create a 2D array to store the minimum edit distance for each substring
+    const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+
+    // Initialize the first row and column
+    for (let i = 0; i <= m; i++) {
+        dp[i][0] = i;
+    }
+    for (let j = 0; j <= n; j++) {
+        dp[0][j] = j;
+    }
+
+    // Iterate through the strings, starting from the second row and column
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            // If the characters at the current indices are the same, the edit distance is the same as the previous substring
+            if (word1[i - 1] === word2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                // Otherwise, the edit distance is the minimum of the following three operations:
+                // 1. Insert a character: dp[i][j - 1] + 1
+                // 2. Delete a character: dp[i - 1][j] + 1
+                // 3. Replace a character: dp[i - 1][j - 1] + 1
+                dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1;
+            }
+        }
+    }
+
+    // Return the minimum edit distance at the bottom-right corner of the dp array
+    return dp[m][n];
 };
