8082f6bbce8618e171b11b94baf5ee9b5a24e615
Sean Teams <shawntemesy@hotmail.com>
2024-08-11 10:59:54 -0300

    Update 139-WorkBreak.js

--- LeetCode/Javascript/100TopLiked/139-WorkBreak.js
+++ LeetCode/Javascript/100TopLiked/139-WorkBreak.js
@@ -1,5 +1,5 @@
 /*
-
+https://leetcode.com/problems/word-break/description/?envType=study-plan-v2&envId=top-100-liked
 Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
 Note that the same word in the dictionary may be reused multiple times in the segmentation.
 
@@ -18,10 +18,10 @@ Example 3:
 Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
 Output: false
 
-Constraints:
+Constraints:    
 1 <= s.length <= 300
 1 <= wordDict.length <= 1000
-1 <= wordDict[i].length <= 20
+1 <= wordDict[i].length <= 20   
 s and wordDict[i] consist of only lowercase English letters.
 All the strings of wordDict are unique.
 */
