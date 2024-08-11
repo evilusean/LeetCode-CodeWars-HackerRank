72ec1296814ace5af3e25495263068469f1b9538
Sean Teams <shawntemesy@hotmail.com>
2024-08-11 11:26:41 -0300

    Update 139-WorkBreak.js

--- LeetCode/Javascript/100TopLiked/139-WorkBreak.js
+++ LeetCode/Javascript/100TopLiked/139-WorkBreak.js
@@ -31,5 +31,25 @@ All the strings of wordDict are unique.
  * @return {boolean}
  */
 var wordBreak = function(s, wordDict) {
-    
+    // Create a boolean array dp of size s.length + 1, where dp[i] indicates whether the substring s[0:i] can be segmented.
+    const dp = new Array(s.length + 1).fill(false);
+    // Initialize dp[0] to true, as an empty string can be segmented.
+    dp[0] = true;
+
+    // Iterate through each index i from 1 to s.length.
+    for (let i = 1; i <= s.length; i++) {
+        // Iterate through each word in the wordDict.
+        for (let j = 0; j < wordDict.length; j++) {
+            // If the current word's length is less than or equal to i and the substring s[i - wordDict[j].length:i] is equal to the current word,
+            // and the substring s[0:i - wordDict[j].length] can be segmented (dp[i - wordDict[j].length] is true),
+            // then the substring s[0:i] can be segmented.
+            if (wordDict[j].length <= i && s.substring(i - wordDict[j].length, i) === wordDict[j] && dp[i - wordDict[j].length]) {
+                dp[i] = true;
+                break; // Break the inner loop as we've found a valid segmentation for s[0:i].
+            }
+        }
+    }
+
+    // Return dp[s.length], which indicates whether the entire string s can be segmented.
+    return dp[s.length];
 };
