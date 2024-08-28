1bfe9d8a9fc1559421e1becddc49dfed45fd63da
Sean Teams <shawntemesy@hotmail.com>
2024-08-28 13:18:00 -0300

    Update 121-BestTimeToBuyAndSellStock.js

--- LeetCode/Javascript/100TopLiked/121-BestTimeToBuyAndSellStock.js
+++ LeetCode/Javascript/100TopLiked/121-BestTimeToBuyAndSellStock.js
@@ -1,5 +1,5 @@
 /*
-
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/?envType=study-plan-v2&envId=top-100-liked
 You are given an array prices where prices[i] is the price of a given stock on the ith day.
 You want to maximize your profit by choosing a single day to buy one stock and 
 choosing a different day in the future to sell that stock.
