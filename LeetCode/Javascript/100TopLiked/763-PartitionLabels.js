2ff427daf3ba297eb31d6887800a76b8ad76344b
Sean Teams <shawntemesy@hotmail.com>
2024-08-29 15:26:02 -0300

    Update 763-PartitionLabels.js

--- LeetCode/Javascript/100TopLiked/763-PartitionLabels.js
+++ LeetCode/Javascript/100TopLiked/763-PartitionLabels.js
@@ -1,7 +1,7 @@
 /*
 https://leetcode.com/problems/partition-labels/description/?envType=study-plan-v2&envId=top-100-liked
-You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part.
-
+You are given a string s. We want to partition the string into as many parts as possible so that each letter 
+appears in at most one part.
 Note that the partition is done so that after concatenating all the parts in order,
 the resultant string should be s.
 Return a list of integers representing the size of these parts.
@@ -27,5 +27,28 @@ s consists of lowercase English letters.
  * @return {number[]}
  */
 var partitionLabels = function(s) {
-    
+    // Create a map to store the last occurrence index of each character in the string
+    const lastIndex = new Map();
+    for (let i = 0; i < s.length; i++) {
+        lastIndex.set(s[i], i);
+    }
+    // Initialize the starting and ending indices of the current partition
+    let start = 0;
+    let end = 0;
+    // Initialize an empty array to store the sizes of the partitions
+    const result = [];
+    // Iterate through the string
+    for (let i = 0; i < s.length; i++) {
+        // Update the ending index of the current partition to the maximum of its current value and the last occurrence index of the current character
+        end = Math.max(end, lastIndex.get(s[i]));
+        // If the current index reaches the ending index of the current partition
+        if (i === end) {
+            // Add the size of the current partition to the result array
+            result.push(end - start + 1);
+            // Update the starting index of the next partition
+            start = end + 1;
+        }
+    }
+    // Return the array of partition sizes
+    return result;
 };
