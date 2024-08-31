8d5fa029a6eafba835c19fa93838c699b04adaca
Sean Teams <shawntemesy@hotmail.com>
2024-08-31 15:05:07 -0300

    Update 49-GroupAnagrams.js

--- LeetCode/Javascript/100TopLiked/49-GroupAnagrams.js
+++ LeetCode/Javascript/100TopLiked/49-GroupAnagrams.js
@@ -1,5 +1,5 @@
 /*
-
+https://leetcode.com/problems/group-anagrams/description/?envType=study-plan-v2&envId=top-100-liked
 Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
 typically using all the original letters exactly once
