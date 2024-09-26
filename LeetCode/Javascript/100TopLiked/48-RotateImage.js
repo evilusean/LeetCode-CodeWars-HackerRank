7d7b04c77b9e00d763344534fc683d3c016d434f
Sean Teams <shawntemesy@hotmail.com>
2024-09-26 00:30:45 -0300

    Update 48-RotateImage.js

--- LeetCode/Javascript/100TopLiked/48-RotateImage.js
+++ LeetCode/Javascript/100TopLiked/48-RotateImage.js
@@ -23,5 +23,17 @@ n == matrix.length == matrix[i].length
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    
+    const n = matrix.length;
+
+    // Transpose the matrix (swap rows and columns)
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+
+    // Reverse each row to complete the 90-degree clockwise rotation
+    for (let i = 0; i < n; i++) {
+        matrix[i].reverse();
+    }
 };
