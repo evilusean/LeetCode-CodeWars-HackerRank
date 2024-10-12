5246537947ba992aee3e4cbd81b3f0ce2900d8e6
Sean Teams <shawntemesy@hotmail.com>
2024-10-12 00:23:10 -0300

    Update 239-SlidingWindowMaximum.js

--- LeetCode/Javascript/100TopLiked/239-SlidingWindowMaximum.js
+++ LeetCode/Javascript/100TopLiked/239-SlidingWindowMaximum.js
@@ -34,5 +34,32 @@ Constraints:
  * @return {number[]}
  */
 var maxSlidingWindow = function(nums, k) {
+    if (nums.length === 0 || k === 0) {
+        return [];
+    }
     
+    let result = [];
+    let deque = []; // Store indices of potential max elements in the window
+
+    for (let i = 0; i < nums.length; i++) {
+        // Remove indices of elements outside the current window
+        while (deque.length > 0 && deque[0] < i - k + 1) {
+            deque.shift();
+        }
+
+        // Remove indices of elements smaller than the current element from the back
+        while (deque.length > 0 && nums[deque[deque.length - 1]] < nums[i]) {
+            deque.pop();
+        }
+
+        // Add the current element's index to the deque
+        deque.push(i);
+
+        // If the window has reached size k, add the max element to the result
+        if (i >= k - 1) {
+            result.push(nums[deque[0]]);
+        }
+    }
+
+    return result;
 };
