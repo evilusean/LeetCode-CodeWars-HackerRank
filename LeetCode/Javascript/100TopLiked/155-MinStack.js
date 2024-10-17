d5b0dd9cff84ff9249571935131ce1346520d3b2
Sean Teams <shawntemesy@hotmail.com>
2024-10-17 19:23:13 -0300

    Update 155-MinStack.js

--- LeetCode/Javascript/100TopLiked/155-MinStack.js
+++ LeetCode/Javascript/100TopLiked/155-MinStack.js
@@ -1,6 +1,40 @@
+/*
+https://leetcode.com/problems/min-stack/description/?envType=study-plan-v2&envId=top-100-liked
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+Implement the MinStack class:
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
 
+Example 1:
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+ 
+
+Constraints:
+-231 <= val <= 231 - 1
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+At most 3 * 104 calls will be made to push, pop, top, and getMin.
+*/
 var MinStack = function() {
-    
+    this.stack = []; // The main stack to store elements
+    this.minStack = []; // A secondary stack to keep track of minimum values
 };
 
 /** 
@@ -8,28 +42,37 @@ var MinStack = function() {
  * @return {void}
  */
 MinStack.prototype.push = function(val) {
-    
+    this.stack.push(val); // Push the value onto the main stack
+    // If the minStack is empty or the new value is less than or equal to the current minimum,
+    // push the new value onto the minStack to maintain the minimum value history.
+    if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]) {
+        this.minStack.push(val);
+    }
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function() {
-    
+    // If the top element of the main stack is equal to the top element of the minStack,
+    // it means we are removing the current minimum, so we pop it from the minStack as well.
+    if (this.stack.pop() === this.minStack[this.minStack.length - 1]) {
+        this.minStack.pop();
+    }
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function() {
-    
+    return this.stack[this.stack.length - 1]; // Return the top element of the main stack
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-    
+    return this.minStack[this.minStack.length - 1]; // Return the top element of the minStack, which represents the current minimum
 };
 
 /** 
