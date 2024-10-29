8222ac2e2333d5b9019f1f47a5cc003cbcc56627
Sean Teams <shawntemesy@hotmail.com>
2024-10-29 07:25:42 -0300

    Update 208-ImplementTrie.js

--- LeetCode/Javascript/100TopLiked/208-ImplementTrie.js
+++ LeetCode/Javascript/100TopLiked/208-ImplementTrie.js
@@ -12,7 +12,6 @@ boolean search(String word) Returns true if the string word is in the trie
     (i.e., was inserted before), and false otherwise.
 boolean startsWith(String prefix) Returns true if there is a previously 
     inserted string word that has the prefix prefix, and false otherwise.
- 
 
 Example 1:
 Input
@@ -36,7 +35,8 @@ word and prefix consist only of lowercase English letters.
 At most 3 * 104 calls in total will be made to insert, search, and startsWith.
 */
 var Trie = function() {
-    
+    this.children = {}; // Hash map to store child nodes, representing characters
+    this.isWordEnd = false; // Flag to indicate if a word ends at this node
 };
 
 /** 
@@ -44,7 +44,15 @@ var Trie = function() {
  * @return {void}
  */
 Trie.prototype.insert = function(word) {
-    
+    let node = this; // Start from the root node
+    for (let i = 0; i < word.length; i++) {
+        const char = word[i];
+        if (!node.children[char]) {
+            node.children[char] = new Trie(); // Create a new node for the character if it doesn't exist
+        }
+        node = node.children[char]; // Move to the child node representing the current character
+    }
+    node.isWordEnd = true; // Mark the current node as the end of a word
 };
 
 /** 
@@ -52,7 +60,15 @@ Trie.prototype.insert = function(word) {
  * @return {boolean}
  */
 Trie.prototype.search = function(word) {
-    
+    let node = this; // Start from the root node
+    for (let i = 0; i < word.length; i++) {
+        const char = word[i];
+        if (!node.children[char]) {
+            return false; // If the character is not found in the trie, the word doesn't exist
+        }
+        node = node.children[char]; // Move to the child node representing the current character
+    }
+    return node.isWordEnd; // Return true if the last node is marked as the end of a word, false otherwise
 };
 
 /** 
@@ -60,7 +76,15 @@ Trie.prototype.search = function(word) {
  * @return {boolean}
  */
 Trie.prototype.startsWith = function(prefix) {
-    
+    let node = this; // Start from the root node
+    for (let i = 0; i < prefix.length; i++) {
+        const char = prefix[i];
+        if (!node.children[char]) {
+            return false; // If the character is not found in the trie, no word starts with the given prefix
+        }
+        node = node.children[char]; // Move to the child node representing the current character
+    }
+    return true; // If we reach this point, it means all characters of the prefix exist in the trie
 };
 
 /** 
