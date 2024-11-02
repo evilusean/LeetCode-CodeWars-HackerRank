d8b50369a2b5cdddd9cf301b7bae28c6967cad16
Sean Teams <shawntemesy@hotmail.com>
2024-11-02 02:31:09 -0300

    Update 31-NextPermutation.js

--- LeetCode/Javascript/100TopLiked/31-NextPermutation.js
+++ LeetCode/Javascript/100TopLiked/31-NextPermutation.js
@@ -39,5 +39,29 @@ Constraints:
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function(nums) {
-    
+    // Find the first decreasing element from the right
+    let i = nums.length - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+        i--;
+    }
+
+    // If we found a decreasing element
+    if (i >= 0) {
+        // Find the smallest element to the right of 'i' that is larger than nums[i]
+        let j = nums.length - 1;
+        while (nums[j] <= nums[i]) {
+            j--;
+        }
+        // Swap nums[i] and nums[j]
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+
+    // Reverse the subarray from i+1 to the end
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+        left++;
+        right--;
+    }
 };
